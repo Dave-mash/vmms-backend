f024b395b4a0f5eb1e8ad29bb572d69ceddf2a91
David Macharia <macharia3041@gmail.com>
2024-09-11 11:53:41 +0300

    Handle user registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Post } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserRepository } from './user.repository';
+import { generateTSID } from 'packages/shared-packages/src/utils';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
+  constructor(private userRepository: UserRepository) {}
+
+  @Post()
+  async createUser(userPayload: CreateAuthDto) {
+    try {
+      console.log(':::::::::: 0.');
+      const { username } = userPayload;
+      const existingUser = await this.userRepository.getUserByUsername(
+        username.toLowerCase(),
+      );
+      console.log(':::::::::: 1.');
+      if (!existingUser) {
+        const payload = {
+          ...userPayload,
+          tsid: generateTSID(),
+          username: username.toLowerCase(),
+        };
+        console.log(':::::::::: 2.');
+        await this.userRepository.createUser(payload);
+      } else {
+        console.log(':::::::::: 3.');
+        const payload = {
+          ...userPayload,
+          username: username.toLowerCase(),
+        };
+        console.log(payload);
+        await this.userRepository.updateUser(username.toLowerCase(), payload);
+      }
+
+      return {
+        msg: 'Success',
+      };
+    } catch (e) {
+      console.log(':::::::::: ', e);
+      throw new BadRequestException('An error occured!');
+    }
+  }
+  create() {
     return 'This action adds a new auth';
   }
 
@@ -16,7 +54,7 @@ export class AuthService {
     return `This action returns a #${id} auth`;
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
+  update(id: number) {
     return `This action updates a #${id} auth`;
   }
 
